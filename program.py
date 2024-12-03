1565acf075544e01a2236200a301afce85fc2d36
Platon <kurbatov.platon2008@gmail.com>
2024-12-03 17:44:30 +0700

    Added the ability to generate multi-colored circles in 255**3 color range

--- program.py
+++ program.py
@@ -1,35 +1,35 @@
 import sys
 from random import randint
-from PyQt6 import uic
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPainter, QPen, QBrush, QColor
 from PyQt6.QtWidgets import QApplication, QWidget
+from UI import Ui_RandomCircles
 
 
-class CirclesGenerator(QWidget):
+class CirclesGenerator(QWidget, Ui_RandomCircles):
     def __init__(self):
         super().__init__()
+        self.setupUi(self)
+
+        self.GenerateButton.clicked.connect(self.add_circle)
+
         self.circles = []
 
-        uic.loadUi('UI.ui', self)
-
-        self.GenerateButton.clicked.connect(self.generate_circles)
-
-    def generate_circles(self):
+    def add_circle(self):
         x = randint(0, self.width())
         y = randint(0, self.height())
-        radius = randint(10, 50)
-        self.circles.append((x, y, radius))
+        radius = randint(10, 100)
+        color = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
+        self.circles.append((x, y, radius, color))
         self.update()
 
     def paintEvent(self, event):
         painter = QPainter(self)
         pen = QPen(QColor("black"), 2, Qt.PenStyle.SolidLine)
-        brush = QBrush(QColor('yellow'))
 
         for circle in self.circles:
             painter.setPen(pen)
-            painter.setBrush(brush)
+            painter.setBrush(QBrush(circle[3]))
             painter.drawEllipse(circle[0], circle[1], circle[2], circle[2])
 
 
